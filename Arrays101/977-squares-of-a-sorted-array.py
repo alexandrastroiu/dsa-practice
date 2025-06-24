88d976e28915958e28a3d5bc91ac50f53ab8c6f6
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-06-24 23:47:21 +0300

    Add solution for Leetcode problem 977
    Added the solution for the Leetcode problem 977.Squares of a Sorted Array
    written in Python.
    
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ Arrays101/977-squares-of-a-sorted-array.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        min_abs = 1e4
+        min_index = 0
+        new_list = []
+
+        for i in range(len(nums)):
+            if abs(nums[i]) < min_abs:
+                min_abs = abs(nums[i])
+                min_index = i
+        
+        new_list.append(nums[min_index]**2)
+        left = min_index - 1
+        right = min_index + 1
+        
+        while left >= 0 or right < len(nums):
+            if left >= 0 and right < len(nums):
+                if abs(nums[left]) <= abs(nums[right]):
+                    new_list.append(nums[left] ** 2)
+                    left -= 1
+                else:
+                    new_list.append(nums[right]**2)
+                    right += 1
+            elif left < 0:
+                new_list.append(nums[right] ** 2)
+                right += 1
+            else:
+                new_list.append(nums[left] ** 2)
+                left -= 1
+                
+            
+        return new_list
