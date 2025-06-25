13036aa2ca15020e3ba9e17b8e66fd63db0fb554
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-06-25 18:43:07 +0300

    Add solution for Leetcode problem 88
    Added the solution for the Leetcode problem 88.Merge Sorted Array in
    Python.
    
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ Arrays101/88-merge-sorted-array.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        left = right = 0
+        new_list = []
+
+        while left < m and right < n:
+            if nums1[left] <= nums2[right]:
+                new_list.append(nums1[left])
+                left += 1
+            else:
+                new_list.append(nums2[right])
+                right += 1
+
+        while left < m:
+            new_list.append(nums1[left])
+            left += 1
+
+        while right < n:
+            new_list.append(nums2[right])
+            right += 1
+
+        for i in range(m + n):
+            nums1[i] = new_list[i]
