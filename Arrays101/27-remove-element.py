81ce20ae004d63f83a92fd38a6e40fbf60cb1a52
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-06-25 18:44:48 +0300

    Add solution for the Leetcode problem 27
    Added the solution for the Leetcode problem 27.Remove Element in Python.
    
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ Arrays101/27-remove-element.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        length = len(nums)
+        left = count = 0
+        right = length - 1
+
+        while left <= right:
+            if nums[left] == val and nums[right] != val:
+                aux = nums[left]
+                nums[left] = nums[right]
+                nums[right] = aux
+
+            if nums[left] != val:
+                left += 1
+
+            if nums[right] == val:
+                right -= 1
+                count += 1
+
+        return length - count
