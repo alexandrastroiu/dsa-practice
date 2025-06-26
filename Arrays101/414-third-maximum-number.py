08a1b784749ac4b417d4458eff02178411c32451
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-06-26 15:07:35 +0300

    Add solution for the leetcode problem 414
    Added the solution for the Leetcode problem 414.Third Maximum Number in
    Python.
    
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ Arrays101/414-third-maximum-number.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        MIN_VALUE = (-2) ** 31 - 1
+        max1 = MIN_VALUE
+        max2 = MIN_VALUE
+        max3 = MIN_VALUE
+
+        for current_element in nums:
+            if current_element > max1:
+                max3 = max2
+                max2 = max1
+                max1 = current_element
+            elif current_element > max2 and current_element != max1:
+                max3 = max2
+                max2 = current_element
+            elif (
+                current_element > max3
+                and current_element != max2
+                and current_element != max1
+            ):
+                max3 = current_element
+
+        if max3 == MIN_VALUE:
+            return max1
+        else:
+            return max3
