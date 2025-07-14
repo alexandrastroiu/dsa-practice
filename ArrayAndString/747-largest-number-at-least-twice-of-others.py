dc243076fdc66f6cab201a4a3364113faec87da5
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-07-14 19:13:50 +0300

    Add solution for the Leetcode problem 747
    Added solution for the Leetcode problem 747 written in Python.
    
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ ArrayAndString/747-largest-number-at-least-twice-of-others.py
@@ -0,0 +1,26 @@
+# Problem: 747. Largest Number At Least Twice of Others
+# Link: https://leetcode.com/problems/largest-number-at-least-twice-of-others/
+
+
+class Solution(object):
+    def dominantIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max1 = max2 = -1
+        max_index = 0
+        length = len(nums)
+
+        for i in range(length):
+            if nums[i] > max1:
+                max2 = max1
+                max1 = nums[i]
+                max_index = i
+            elif nums[i] > max2:
+                max2 = nums[i]
+
+        if max1 >= (2 * max2):
+            return max_index
+        else:
+            return -1
