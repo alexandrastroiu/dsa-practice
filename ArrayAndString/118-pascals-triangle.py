46742624302f876c2359ac8853a6daac51153476
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-07-15 19:16:40 +0300

    Add solution for the Leetcode problem 118
    Added solution for the Leetcode problem 118.Pascal's Triangle written in
    Python.
    
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ ArrayAndString/118-pascals-triangle.py
@@ -0,0 +1,32 @@
+# Problem: 118. Pascal's Triangle
+# Link: https://leetcode.com/problems/pascals-triangle/
+
+
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        result = []
+        count = 3
+
+        if numRows >= 1:
+            result.append([1])
+        if numRows >= 2:
+            result.append([1, 1])
+
+        while count <= numRows:
+            temp_list = []
+            temp_list.append(1)
+            length = len(result)
+
+            for i in range(1, len(result[length - 1])):
+                sum_above = result[length - 1][i - 1] + result[length - 1][i]
+                temp_list.append(sum_above)
+
+            temp_list.append(1)
+            result.append(temp_list)
+            count += 1
+
+        return result
