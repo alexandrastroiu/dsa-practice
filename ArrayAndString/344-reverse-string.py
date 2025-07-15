f4f1b42e7bc52a0b3ae47cdb36fa6d5b5f40e155
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-07-15 19:30:07 +0300

    Add solution for the Leetcode problem 344
    Added solution for the Leetcode problem 344 in Python.
    
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ ArrayAndString/344-reverse-string.py
@@ -0,0 +1,20 @@
+# Problem: 344. Reverse String
+# Link: https://leetcode.com/problems/reverse-string/
+
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        left = 0
+        right = len(s) - 1
+
+        while left < right:  # use two-pointer technique
+            aux = s[left]  # swap
+            s[left] = s[right]
+            s[right] = aux
+
+            left += 1
+            right -= 1  # O(1) extra space
