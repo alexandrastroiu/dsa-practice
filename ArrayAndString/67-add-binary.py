4321297723277a4af2f809bc11e02dbaa769f80c
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-07-15 19:04:51 +0300

    Add solution for the Leetcode problem 67
    Added solution for the Leetcode problem 67.Add Binary written in Python.
    
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ ArrayAndString/67-add-binary.py
@@ -0,0 +1,44 @@
+# Problem: 67. Add Binary
+# Link: https://leetcode.com/problems/add-binary/
+
+
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        length_a = len(a)
+        length_b = len(b)
+        i = length_a - 1
+        j = length_b - 1
+        carry = 0
+        binary_sum = []
+        
+        while i >= 0 or j >= 0:
+            
+            if i >= 0 and j >= 0:
+                current_sum = ((int(a[i]) + int(b[j])) % 2 + carry) % 2
+                carry = (int(a[i]) & int(b[j])) | (int(a[i]) & carry) | (int(b[j]) & carry) 
+                #use bitwise operators
+            elif i >= 0:
+                current_sum = (int(a[i]) + carry) % 2
+                carry = int(a[i]) & carry
+            elif j >= 0:
+                current_sum = (int(b[j]) + carry) % 2
+                carry = int(b[j]) & carry
+                
+            binary_sum.append(str(current_sum))
+            i -= 1
+            j -= 1
+        
+        if carry == 1:
+            binary_sum.append(str(carry))
+        
+        binary_sum.reverse()
+        return "".join(binary_sum)
+            
+
+        
+        
