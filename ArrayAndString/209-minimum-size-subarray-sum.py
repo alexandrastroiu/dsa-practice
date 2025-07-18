5f2a7f212b4ae3e5be07201f1a19bf1deaa26e95
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-07-18 12:55:25 +0300

    Add solution for Leetcode problem 209
    Added the solution for the Leetcode problem 209.Minimum Size Subarray Sum
    in Python.
    
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ ArrayAndString/209-minimum-size-subarray-sum.py
@@ -0,0 +1,25 @@
+# Problem: 209. Minimum Size Subarray Sum
+# Link: https://leetcode.com/problems/minimum-size-subarray-sum/
+
+
+class Solution(object):
+    def minSubArrayLen(self, target, nums):
+        """
+        :type target: int
+        :type nums: List[int]
+        :rtype: int
+        """
+        # using the Sliding Window technique
+        left = right = current_sum = 0
+        length = len(nums)
+        minimal_length = float("inf")
+
+        for right in range(length):
+            current_sum += nums[right]
+
+            while current_sum >= target:
+                minimal_length = min(minimal_length, right - left + 1)
+                current_sum -= nums[left]
+                left += 1
+
+        return minimal_length if minimal_length != float("inf") else 0
